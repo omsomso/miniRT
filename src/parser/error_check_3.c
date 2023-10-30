77bfcdae35b17596c8dbac600622ade54b63a322
omsomso <pkrystian.k@gmail.com>
2023-10-30 11:38:09 +0100

    no ambient light error message fix

--- src/parser/error_check_3.c
+++ src/parser/error_check_3.c
@@ -6,7 +6,7 @@
 /*   By: kpawlows <kpawlows@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 18:44:28 by kpawlows          #+#    #+#             */
-/*   Updated: 2023/10/26 00:48:33 by kpawlows         ###   ########.fr       */
+/*   Updated: 2023/10/30 11:33:45 by kpawlows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@ int	check_scene(t_data *data)
 {
 	if (!data->ambient_light)
 	{
-		write(2, "Error\nNo ambient light\n", 25);
+		write(2, "Error\nNo ambient light\n", 24);
 		exit(1);
 	}
 	if (!data->camera)
